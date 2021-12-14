9dd9676675aa6da51f8cf02d5d0e306ffb76c694
Augusto <fenkzebra147@gmail.com>
2021-12-13 21:40:55 -0300

    Comentário do commit da minha nova branch

---
+++ exercicios/celsius_para_fahrenheit.py
@@ -0,0 +1,8 @@
+""" 
+Exercicio de conversao de graus celsius em Fahrenheit, utilizando Python.
+"""
+
+c = float(input('Qual a temperatura em graus Celsius: '))
+f = ''
+f = c * ( 9 / 5 ) + 32
+print(f'A temperatura em graus Fahrenheit e de: {f}')
